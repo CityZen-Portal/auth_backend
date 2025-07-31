52c9dec2ad705dded06d5f6160fbdd26a09e6f86
Siranjeevi619 <siranjeevi0619@gmail.com>
2025-07-31 19:53:45 +0530

    fix: added count api

---
+++ src/main/java/com/cityzen/auth/AuthApplication.java
@@ -0,0 +1,10 @@
+package com.cityzen.auth;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+        System.out.println("Server started");
+    }
+}
